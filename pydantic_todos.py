9c14a6ce5492f218a1dea90aa685d2135d62bbce
sajib925 <sajibahmed1004@gmail.com>
2026-09-20 19:46:13 +0600

    first commit

---
+++ pydantic_todos.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class TodoPydantic(BaseModel):
+    title : str = Field(max_length=40)
+    description : str = Field(max_length=200)
+    priority : int = Field(ge=0, le=5)
+    completed : bool
+    
+class TodoPydanticUpdate(BaseModel):
+    title : Optional[str] = Field(default=None, max_length=40)
+    description : Optional[str] = Field(default=None, max_length=200)
+    priority : Optional[int] = Field(default=None, ge=0, le=5)
+    completed : Optional[bool] = Field(default=None)
+
+
+class UserPydantic(BaseModel):
+    email: str
+    username: str
+    firstname: str
+    lastname: str
+    password: str
+    is_active: bool
+    role: str
