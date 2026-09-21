95d4ad87f1b44c16c26fd72d046ddaae798660cf
sajib925 <sajibahmed1004@gmail.com>
2026-09-21 19:59:24 +0600

    add neon db'

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL="postgresql://postgres:postgres@localhost:5432/todos"
+# SQLALCHEMY_DATABASE_URL="postgresql://postgres:postgres@localhost:5432/todos"
+SQLALCHEMY_DATABASE_URL="postgresql://neondb_owner:npg_UQtx02NXySRm@ep-quiet-mountain-aw28uu4r-pooler.c-12.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
